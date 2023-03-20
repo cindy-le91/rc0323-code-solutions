c8fa09bc9feec1b26d8232131d2b7261da9439ef
Cindy Le <cindee.le@gmail.com>
2023-03-20 21:03:39 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+const width = 6;
+const height = 8;
+const area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 9;
+const payment = 12;
+const change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+const quizzes = 87;
+const midterm = 93;
+const final = 78;
+const grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Cindy';
+const lastName = 'Le';
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 8;
+const isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const headCount = 11;
+const isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+const motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
