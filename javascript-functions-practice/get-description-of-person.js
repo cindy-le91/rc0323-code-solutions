073b469f9f123bb86894d5f9901f9b0573b67984
Cindy Le <cindee.le@gmail.com>
2023-03-21 23:22:53 +0000

    Completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-description-of-person.js
@@ -0,0 +1,5 @@
+/* exported getDescriptionOfPerson */
+function getDescriptionOfPerson(person) {
+  const getDescriptionOfPerson = person.name + ' is a ' + person.occupation + ' from ' + person.birthPlace + '.';
+  return getDescriptionOfPerson;
+}
