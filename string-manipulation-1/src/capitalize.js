5b18bc790c8d2001b4a622df6f0b87e33c4787f5
Cindy Le <cindee.le@gmail.com>
2023-03-23 22:03:57 +0000

    Updated string manipulation 1 with pseudocode.

--- string-manipulation-1/src/capitalize.js
+++ string-manipulation-1/src/capitalize.js
@@ -1,4 +1,7 @@
 /* exported capitalize */
+
+// define a function, named 'capitalized' that accepts one argument 'word'.
+// take the first letter of the word and uppercase it; take the rest of the word and lowercase it. combine the strings and return
 function capitalize(word) {
   return word[0].toUpperCase() + word.substring(1).toLowerCase();
 }
--- string-manipulation-1/src/get-last-char.js
+++ string-manipulation-1/src/get-last-char.js
@@ -1,4 +1,7 @@
 /* exported getLastChar */
+
+// define a new function named 'getLastChar' that takes one argument 'string'
+// get last character of each string and return
 function getLastChar(string) {
   return string[string.length - 1];
 }
