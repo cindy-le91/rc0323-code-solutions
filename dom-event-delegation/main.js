e7fabb5ec1306f3c5c3d1628c341a13e461bda06
Cindy Le <cindee.le@gmail.com>
2023-04-06 18:19:41 +0000

    updated solution

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -5,6 +5,6 @@ taskList.addEventListener('click', function (event) {
   console.log('event.target.tagName: ', event.target.tagName);
   const button = event.target;
   if (event.target && event.target.matches('button')) {
-    console.log('closest .task-list-item: ', button.closest('.task-list-item'));
+    console.log('closest .task-list-item: ', button.closest('.task-list-item').remove());
   }
 });
