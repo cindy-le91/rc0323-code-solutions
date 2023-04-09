6970e6bc804f4dfbb6af562ec6d03297cdca8006
Cindy Le <cindee.le@gmail.com>
2023-04-09 03:25:20 +0000

    completed object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,14 @@
 /* exported defaults */
+// define a function named defaults that takes two arguments, 'target' and 'source
+function defaults(target, source) {
+// loop through source object
+  for (const key in source) {
+    // if target object already has property matching the source key
+    if (Object.prototype.hasOwnProperty.call(target, key)) {
+      // skip over
+      continue;
+    }
+    // add source's property to target object
+    target[key] = source[key];
+  }
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,17 @@
 /* exported omit */
+// define a function named 'omit' tha takes two arguments, 'source' and 'keys'
+function omit(source, keys) {
+  // makes a new object that is identical to the object parameter. this is to be able to delete.
+  const newObject = Object.assign({}, source);
+
+  // loop through the keys
+  for (const index in keys) {
+    // if object has the property matching key
+    if (newObject[keys[index]]) {
+      // delete the property
+      delete newObject[keys[index]];
+    }
+  }
+
+  return newObject;
+}
