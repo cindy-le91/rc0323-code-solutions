587851c21efd63803159dc20282e064a99a5f2de
Cindy Le <cindee.le@gmail.com>
2023-05-23 18:04:40 +0000

    completed react-hot-button-component exercise

---
+++ react-hot-button-component/my-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          <Button color="first" text='Hot Button' />
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
