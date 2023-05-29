e65110d742a5ef736db33c19b6d66c60331f8d2a
Cindy Le <cindee.le@gmail.com>
2023-05-29 07:04:59 +0000

    completed fetch exercise

--- fetch/main.js
+++ fetch/main.js
@@ -1,6 +1,10 @@
 async function getData() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error('there was error');
+    }
+
     const jsonData = await response.json();
     console.log(jsonData);
   } catch (error) {
@@ -13,6 +17,10 @@ getData();
 async function getPokemon() {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+    if (!response.ok) {
+      throw new Error('there was error');
+    }
+
     const jsonData = await response.json();
     console.log(jsonData);
   } catch (error) {
